7e7fb619275d85b7a373ae88b58bab8d578ae487
Growler42 <tarrrasio@gmail.com>
2022-03-31 16:17:24 +0300

    feat: add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, BrowserAnimationsModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from './card/card.component';
+import { MatCardModule } from '@angular/material/card';
+
+const materialModules = [MatCardModule];
 
 @NgModule({
   declarations: [CardComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ...materialModules],
+  exports: [CardComponent, ...materialModules],
 })
 export class SharedModule {}
