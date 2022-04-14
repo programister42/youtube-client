14d5790a6605b119a867824571f4543285872ca2
Taras Tereschenko <tarrrasio@gmail.com>
2022-04-14 17:22:28 +0300

    feat: implement search on typing

--- src/app/core/components/serch-bar/search-bar.component.ts
+++ src/app/core/components/serch-bar/search-bar.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, filter, map, Observable } from 'rxjs';
 import { SearchMockupService } from '../../services/search-mockup.service';
 
 @Component({
@@ -7,12 +8,29 @@ import { SearchMockupService } from '../../services/search-mockup.service';
 	templateUrl: './search-bar.component.html',
 	styleUrls: ['./search-bar.component.scss'],
 })
-export class SearchBarComponent {
+export class SearchBarComponent implements OnInit {
 	searchForm: FormGroup = new FormGroup({
 		searchInput: new FormControl(''),
 	});
 
-	constructor(private searchService: SearchMockupService) {}
+	searchStream$: Observable<string> = this.searchForm.valueChanges.pipe(
+		map((value) => value.searchInput),
+		filter((value) => value.length > 2),
+		debounceTime(500),
+		distinctUntilChanged(),
+	);
+
+	constructor(private searchService: SearchMockupService) {
+		this.searchStream$.subscribe((value) => {
+			console.log(value);
+		});
+	}
+
+	ngOnInit(): void {
+		this.searchStream$.subscribe((value) => {
+			this.searchService.search(value);
+		});
+	}
 
 	onSubmit(e: Event): void {
 		e.preventDefault();
